d499f3342cc24817d140ad6569672fe5c6c622e8
David Hatten <dhatten87@gmail.com>
2016-09-13 17:43:14 -0400

    Added the necessary classes for the assignement

---
+++ assignment1/WineItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment1
+{
+    class WineItem
+    {
+        /*Backing Fields*/
+        private string _description;
+        private Int32 _id;
+        private string _pack;
+
+        /*Properties*/
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public Int32 ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Pack
+        {
+            get { return _pack; }
+            set { _pack = value; }
+        }
+
+        /*Constructors*/
+        public WineItem(Int32 id, string description, string pack)
+        {
+            this._id = id;
+            this._description = description;
+            this._pack = pack;
+        }
+        public WineItem()
+        {
+            /*Default Constructor*/
+        }
+
+        /*Public Methods*/
+        public override string ToString()
+        {
+            return this._id + " " + this._description + " " + this._pack;
+        }
+    }
+}
+
